de3f671c9fdae2dea694b3b77967ceb5f27868d7
Ramdan Rohendi <ramdanrohendi@gmail.com>
2020-09-24 07:58:46 +0700

    Latihan Membuat Fragment

---
+++ java/ProfileFragment.java
@@ -0,0 +1,26 @@
+package com.example.fragmentbuttonnavigation;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fragmentbuttonnavigation.R;
+
+public class ProfileFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_profile, null);
+    }
+
+}
